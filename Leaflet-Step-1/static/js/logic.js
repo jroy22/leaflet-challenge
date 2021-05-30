851894738f05a77c6c74b7504cc2dc8ddd6a5784
Jeannaej <jeannaejy22@gmail.com>
2021-05-29 21:45:45 -0500

    created a create map function

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -10,7 +10,7 @@ var myMap = L.map("map",
     );
   
   // Adding a tile layer (the background map image) to our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var baseMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -19,11 +19,40 @@ var myMap = L.map("map",
     accessToken: API_KEY
   }).addTo(myMap);
 
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+// Store API endpoint inside queryUrl - Weekly Earthquake data
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl).then(function(weekearthquakedata) {
+d3.json(queryUrl).then(function(weeklyearthquakedata) {
   // Once we get a response, send the data.features object to the createFeatures function
-  console.log(weekearthquakedata);
+  console.log(weeklyearthquakedata);
 });
+
+function createMap(earthquakes) {
+     // Create overlay object to hold our overlay layer
+     var overlayMaps = {
+        Earthquakes: earthquakes
+     };
+
+    // Adding a tile layer (the background map image) to map
+    var baseMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v10",
+        accessToken: API_KEY
+        }).addTo(myMap);
+
+    // Creating our initial map object
+    var myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 4.5,
+        layers: [baseMap,overlayMaps]
+        });
+    // Create a layer control   
+    L.control.layers(baseMap, overlayMaps, {
+        collapsed: false
+        }).addTo(myMap);
+  };
+

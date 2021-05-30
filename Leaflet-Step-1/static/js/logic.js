715d15fd1bc1f122e839510191672f8ba26f5b85
Jeannaej <jeannaejy22@gmail.com>
2021-05-29 23:48:26 -0500

    Added code to add color to circle markers and resize base on the depth

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -32,19 +32,64 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(earthquakedata) {
     console.log(earthquakedata);
+    var mColors = ["#ff5f65", "#fca35d", "#fbd72a", "#f7db11", "#dcf400", "#a3f600"]
 
-L.geoJSON(earthquakedata, {
-    pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-    },
-    //style: 
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +
-        "</h3><h3>Depth: " + feature.geometry.coordinates[2] + "</h3><hr><p>" + feature.properties.place + "<p>");
+    // Function to determine the marker color
+    function markerColors(depth) {
+        var colorFill = "white";
+        switch(true){
+            case depth > 90:
+                colorFill = mColors[0];
+                break;
+    
+            case depth > 70:
+                colorFill = mColors[1];
+                break;
+    
+            case depth > 50:
+                colorFill = mColors[2];
+                break;
+    
+            case depth > 30:
+                colorFill = mColors[3];
+                break;
+    
+            case depth > 10:
+                colorFill = mColors[4];
+                break;
+    
+            case depth > -10:
+                colorFill = mColors[5];
+                break;
+        }
+        return colorFill;
     }
 
-}).addTo(earthquakes);
-earthquakes.addTo(myMap);
+    function markerStyle(features){
+        return {
+            opacity: 1, 
+                fillOpacity: 1,
+                fillColor: markerColors(features.geometry.coordinates[2]),
+                color: "#FFFFFF",
+                radius: 5 * features.properties.mag,
+                stroke: true,
+                weight: 0.5
+        };
+    }
 
+    L.geoJSON(earthquakedata, {
+        pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+        },
+
+        style: markerStyle,
+
+        onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +
+        "</h3><h3>Depth: " + feature.geometry.coordinates[2] + "</h3><hr><p>" + feature.properties.place + "<p>");
+        }
+
+    }).addTo(earthquakes);
+    earthquakes.addTo(myMap);
 
 });

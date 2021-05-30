e098bfd68024da352c79af8f8a0f1759e0c55543
Jeannaej <jeannaejy22@gmail.com>
2021-05-29 23:15:14 -0500

    Removed functions and redid the mapping in order to get the markers to show up

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,68 +1,50 @@
 // Check to see if the file is being read
 console.log("Logic File Loaded");
 
+var earthquakes = new L.LayerGroup();
+
+// Create overlay object to hold our overlay layer
+var overlayMaps = {Earthquakes: earthquakes};
+
+// Define a baseMaps object to hold base layers
+var baseMaps = {"Light map": lightmap};
+
+// Adding a tile layer (the background map image) to map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    });
+
+// Creating our initial map object
+var myMap = L.map("map", {
+    center: [0,0],
+    zoom: 2,
+    layers: [lightmap,earthquakes]
+    });
+
 // Store API endpoint inside queryUrl - Weekly Earthquake data
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  console.log(data);
-  createFeatures(data.features);
-});
+d3.json(queryUrl).then(function(earthquakedata) {
+    console.log(earthquakedata);
 
-function createFeatures(earthquakeData) {
-
-    function pointToLayer(feature, latlng){
+L.geoJSON(earthquakedata, {
+    pointToLayer: function(feature, latlng) {
         return L.circleMarker(latlng);
-    }
-
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing 
-    function onEachFeature(feature, layer) {
+    },
+    //style: 
+    onEachFeature: function(feature, layer) {
         layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +
         "</h3><h3>Depth: " + feature.geometry.coordinates[2] + "</h3><hr><p>" + feature.properties.place + "<p>");
     }
-  
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature,
-      pointToLayer: pointToLayer,
-    //   style: style
-    });
-  
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
-}
 
-function createMap(earthquakes) {
-     // Create overlay object to hold our overlay layer
-     var overlayMap = {
-        Earthquakes: earthquakes
-     };
-
-    // Adding a tile layer (the background map image) to map
-    var baseMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v10",
-        accessToken: API_KEY
-        });
-
-    // Creating our initial map object
-    var myMap = L.map("map", {
-        center: [0,0],
-        zoom: 2,
-        layers: [baseMap,overlayMap]
-        });
-    // Create a layer control   
-    L.control.layers(baseMap, overlayMaps, {
-        collapsed: false
-        }).addTo(myMap);
-  };
+}).addTo(earthquakes);
+earthquakes.addTo(myMap);
 
 
-
+});
